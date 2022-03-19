61d2c7423339f7d2a99d5b6be176fda137f5da8a
vlstam <100720483+vlstam@users.noreply.github.com>
2022-03-19 18:35:17 +0200

    Update Lecture6Homework2.java

--- Lecture6Homework2.java
+++ Lecture6Homework2.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 public class Lecture6Homework2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num;
+        float num;
         System.out.println("Please enter a number: ");
         num = scan.nextInt();
         String isOddOrEven = num%2==0 ? "even" : "odd";
-        System.out.printf("Is %d even or odd?\n", num);
+        System.out.printf("Is %f even or odd?\n", num);
         System.out.println("It is " + isOddOrEven);
 
 
